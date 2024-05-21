2669669187ec9354c8a7752ccc569f9e36d8a3c8
oceanumeric <numerical.ocean@gmail.com>
2024-05-21 22:11:51 +0200

    [update]

--- app/api/routes/api.py
+++ app/api/routes/api.py
@@ -6,7 +6,6 @@ from app.api.routes import get_review_by_id
 from app.api.routes import semantic_query
 
 router = APIRouter()
-router.include_router(predictor.router, tags=["predictor"], prefix="/v1")
 router.include_router(upload_review.router, tags=["upload"], prefix="/v1")
 router.include_router(get_review_by_id.router, tags=["get"], prefix="/v1")
 router.include_router(semantic_query.router, tags=["semantic query"], prefix="/v1")
